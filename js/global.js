33ba62dcc71731f5c188f54180a32548e8f158e2
Ksardy <zloiivanushka@gmail.com>
2018-08-26 16:45:05 +0300

    еще раз

--- js/global.js
+++ js/global.js
@@ -1,2 +1,8 @@
-
-
+import './utils.js';
+import './main.js';
+import './welcome.js';
+import './game-genre.js';
+import './game-artist.js';
+import './result-success.js';
+import './fail-tries.js';
+import './fail-time.js';
