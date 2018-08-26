110cec4e396d34e3d0a551e14e509296fd2170e9
Ksardy <zloiivanushka@gmail.com>
2018-08-26 11:38:10 +0300

    вроде чтото сделал

--- js/global.js
+++ js/global.js
@@ -1,9 +1,2 @@
 
-import './utils.js';
-import './main.js';
-import './welcome.js';
-import './game-genre.js';
-import './game-artist.js';
-import './result-success.js';
-import './fail-tries.js';
-import './fail-time.js';
+
